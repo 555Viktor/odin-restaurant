e8375efecd3e4a0b75845d77731b1877ed792861
555Viktor <victor.vaskov2002@gmail.com>
2025-01-22 10:59:49 +0200

    Improve clarity in index.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,17 @@ import {createHomePage} from "./home-page";
 import {createContactPage} from "./contact-page";
 import {createMenuPage} from "./menu-page";
 
-export {loadMenuPage, loadContactPage, clearContentContainer};
-
+// Variables for DOM elements
 const contentDiv = document.getElementById('content');
 
 const navHomeBtn = document.querySelector('.nav-btn-home');
 const navMenuBtn = document.querySelector('.nav-btn-menu');
 const navContactBtn = document.querySelector('.nav-btn-contact');
 
-
+// Helper functions for clearing content div and appending pages
 function clearContentContainer () {
     contentDiv.innerHTML = '';
-}
+};
 
 function loadHomePage () {
     contentDiv.appendChild(createHomePage());
@@ -28,20 +27,24 @@ function loadContactPage () {
     contentDiv.appendChild(createContactPage());
 };
 
-
+// Navigation menu event listeners for page loading
 navHomeBtn.addEventListener('click', () => {
-    clearContentContainer()
-    loadHomePage()
-})
+    clearContentContainer();
+    loadHomePage();
+});
 
 navMenuBtn.addEventListener('click', () => {
     clearContentContainer();
     loadMenuPage();
-})
+});
 
 navContactBtn.addEventListener('click', () => {
     clearContentContainer()
     loadContactPage();
-})
+});
 
-loadHomePage()
+// Default page
+loadHomePage();
+
+// Exporting to home-page.js for home-page btn event listeners
+export {loadMenuPage, loadContactPage, clearContentContainer};
